a3fecd075f146a3f228fc7a34267162763fdd337
seongjongju <sjj990480@gmail.com>
2025-02-14 15:35:18 +0900

    Two

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
-const textAnimation = () => {
-  const divs = document.querySelectorAll('.text-container div');
-  const textContainer = document.querySelector('.text-container');
-
-  window.addEventListener('scroll', () => {
+//한문장씩
+  const textAnimation = () => {
+    const divs = document.querySelectorAll('.text-container div');
     const windowHeight = window.innerHeight;
 
-    const divsTop = () => {
-      divs.forEach((div) => {
-        const divTop = div.getBoundingClientRect().top;
-        const divBottom = div.getBoundingClientRect().bottom;
+    divs.forEach((div) => {
+      const divTop = div.getBoundingClientRect().top;
+      const divBottom = div.getBoundingClientRect().bottom;
+      if(divTop >= 0 && divBottom <= windowHeight) {
+        div.classList.add('on');
+      } else {
+        div.classList.remove('on');
+      }
+    });
+  };
 
-        if(divTop >= 0 && divBottom <= windowHeight) {
-          div.classList.add('on');
-        } else {
-          div.classList.remove('on');
-        }
-      });
-    };
+  window.addEventListener('scroll', textAnimation);
+//한문장씩
 
-    divsTop();
+//한글자씩
+const textAnimationTwo = () => {
+  const oneTexts = document.querySelectorAll('.letter_container div');
+  const windowHeight = window.innerHeight;
+  console.log(oneTexts);
+
+  oneTexts.forEach((oneText, oneTextIndex) => {
+    const oneTextTop = oneText.getBoundingClientRect().top;
+    const oneTextBottom = oneText.getBoundingClientRect().bottom;
+
+    if(oneTextTop >= 0 && oneTextBottom <= windowHeight) {
+      setTimeout(() => {
+        oneText.classList.add('on');
+      }, oneTextIndex * 20);
+    } else {
+      oneText.classList.remove('on');
+    }
   });
-
 };
 
-textAnimation();
-
+window.addEventListener('scroll', textAnimationTwo);
+//한글자씩
